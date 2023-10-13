57fff6ca3bdf206d5408477c74a9363f745ee95c
Tim Harding <tim@timharding.co>
2023-10-12 20:36:03 -0700

    Set up with trunk and wasm-pack testing

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+pub mod tests {
+    use wasm_bindgen_test::*;
+    wasm_bindgen_test_configure!(run_in_browser);
+
+    #[wasm_bindgen_test]
+    fn it_works() {
+        assert_eq!(1, 1);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::panic;
+use wasm_bindgen::prelude::*;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 fn main() {
     println!("Hello, world!");
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+    panic!("Panicking aaaaa");
 }
