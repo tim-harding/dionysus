4bd7fc298932a0f482fb77edd1c49fc9c31d7dec
Tim Harding <Tim@TimHarding.co>
2025-01-15 12:36:16 -0800

    Collapse UnsafeElement

--- src/view.rs
+++ src/view.rs
@@ -23,19 +23,15 @@ use bevy_ecs::prelude::*;
 use bevy_utils::synccell::SyncCell;
 use web_sys::HtmlElement;
 
-#[derive(Component)]
-struct Element(UnsafeElement);
-
 /// A wrapper around [`HtmlElement`] that implements [`Send`] and [`Sync`]. This is okay in
 /// practice because WASM is only ever single-threaded, although this is something to pay attention
 /// to if threading comes to WASM. Also need to pay attention and clone HtmlElement in public APIs
 /// instead of handing out references to the inner type, which would be problematic in the presence
 /// of any future threading API.
-struct UnsafeElement(HtmlElement);
-unsafe impl Send for UnsafeElement {}
-unsafe impl Sync for UnsafeElement {}
-
-pub struct SignalId(u32);
+#[derive(Component)]
+struct Element(HtmlElement);
+unsafe impl Send for Element {}
+unsafe impl Sync for Element {}
 
 #[derive(Debug, Default)]
 pub struct Cult {
