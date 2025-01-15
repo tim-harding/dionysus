72771f17387b9fb31a1cbb2732bbe138d942e246
Tim Harding <Tim@TimHarding.co>
2025-01-15 00:41:46 -0800

    Stubbing out Cult

--- src/view.rs
+++ src/view.rs
@@ -18,3 +18,30 @@
 // Bindings), etc. to perform fine-grained updates. User code creates DOM fragments and updates
 // Slot, TextContent, etc., then ECS reactivity system picks up changes and applies them to the
 // DOM.
+
+use bevy_ecs::prelude::*;
+use bevy_utils::synccell::SyncCell;
+use web_sys::HtmlElement;
+
+#[derive(Component)]
+struct El(UnsafeElement);
+
+/// A wrapper around [`HtmlElement`] that implements [`Send`] and [`Sync`]. This is okay in
+/// practice because WASM is only ever single-threaded, although this is something to pay attention
+/// to if threading comes to WASM. Also need to pay attention and clone HtmlElement in public APIs
+/// instead of handing out references to the inner type, which would be problematic in the presence
+/// of any future threading API.
+struct UnsafeElement(HtmlElement);
+unsafe impl Send for UnsafeElement {}
+unsafe impl Sync for UnsafeElement {}
+
+#[derive(Debug, Default)]
+pub struct Cult {
+    world: World,
+}
+
+impl Cult {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
